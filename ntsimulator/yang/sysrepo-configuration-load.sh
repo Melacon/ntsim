f1d91e2b89f5167c0af097909b143e6ba7f935fc
Alex Stancu <alexandru.stancu@highstreet-technologies.com>
2020-03-11 18:30:11 +0200

    Fix bug where random data generation does not stop even after configuration is loaded in the docker containers.

--- ntsimulator/yang/sysrepo-configuration-load.sh
+++ ntsimulator/yang/sysrepo-configuration-load.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-################################################################################
-#
-# Copyright 2019 highstreet technologies GmbH and others
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-################################################################################
 
 sleep 20
 
@@ -39,8 +23,11 @@ result=$(netopeer2-cli <<-END
 END
 )
 
-while [[ "$result" != "OK" ]]
+count=1
+
+while [[ $count -le 20 ]] && [[ "$result" != "OK" ]]
 do
+  ((count++))
   pyang -f sample-xml-skeleton --sample-xml-list-entries 2 *.yang
   
   result=$(netopeer2-cli <<-END
